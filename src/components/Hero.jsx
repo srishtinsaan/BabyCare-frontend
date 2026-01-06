e2f81a6a4aa66524f26b54e14421e5b8347ea875
Srishti Singh <srish17816@gmail.com>
2026-01-06 11:09:44 +0530

    hero page fetch details

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
 
 function Hero() {
+
+  const [heading, setHeading] = useState("About Us");
+  const [subHeading, setSubHeading] = useState(
+    "We Learn Smart Way To Build Bright Future For Your Children"
+  );
+  const [bgImage, setBgImage] = useState(
+    "https://themewagon.github.io/BabyCare/img/hero-img.jpg"
+  ); // default
+
+
+   useEffect(() => {
+    async function fetchHomeData() {
+      try {
+        const response = await fetch(
+          "https://babycare-admin-backend-ulfg.onrender.com/home", {cache: "no-store"}
+        )
+        const json = await response.json();
+
+        if (json.success && json.data) {
+          const data = json.data;
+          setHeading(data.heading || heading);
+          setSubHeading(data.subHeading || subHeading);
+          setBgImage(data.bgImageUrl || bgImage);
+        }
+      } catch (error) {
+        console.log("Error fetching About data:", error);
+      }
+    }
+
+    fetchHomeData();
+  }, []);
+
+
   return (
     <div className="w-full h-screen overflow-hidden relative">
 
 
       {/* Background Image */}
       <img
-        src="https://themewagon.github.io/BabyCare/img/hero-img.jpg"
+        src={bgImage}
         className="w-full h-full object-cover opacity-90"
       />
 
@@ -18,12 +50,12 @@ function Hero() {
         
         <p style={{ fontFamily: "'Montserrat', sans-serif", fontWeight: 300 }}
           className="text-[#F4467B] text-3xl">
-          We Care Your Baby
+          {heading}
         </p>
 
         <h1 style={{ fontFamily: "'Fredoka', sans-serif", fontWeight: 700 }}
           className="text-white text-6xl leading-tight max-w-2xl">
-          The Best Play Area For Your Kids
+          {subHeading}
         </h1>
 
         {/* Buttons */}
